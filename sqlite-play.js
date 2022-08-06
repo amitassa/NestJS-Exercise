cea8df52c2226b91402fbc0b4db7f39f2ce405f1
Amit Assa <amitassa9@gmail.com>
2022-08-06 21:03:11 +0300

    Delete and update added

--- sqlite-play.js
+++ sqlite-play.js
@@ -18,7 +18,32 @@ const db = new sqlite3.Database("./mock.db", sqlite3.OPEN_READWRITE, (err)=>{
 
 // insert data into table
 
-sql = 'INSERT INTO users (first_name, last_name, username, password) VALUES (?,?,?,?)'
-db.run(sql, ["amit", "assa", "kingston", "12345"], (err) => {
+// sql = 'INSERT INTO users (first_name, last_name, username, password) VALUES (?,?,?,?)'
+// db.run(sql, ["amit", "assa", "kingston", "12345"], (err) => {
+//     if (err) return console.error(err.message);
+// })
+
+
+// update 
+
+// sql = 'UPDATE users SET first_name = ? WHERE id = ?';
+// db.run(sql, ["Jake", 1], (err)=>{
+//     if (err) return console.error(err.message);
+// });
+
+// Delete
+
+sql = 'DELETE FROM users WHERE id = ?';
+db.run(sql, [1], (err)=>{
     if (err) return console.error(err.message);
+});
+
+// print rows
+
+sql = 'SELECT * FROM users'
+db.all(sql, [], (err, rows) =>{
+    if (err) return console.error(err.message)
+    rows.forEach((row) =>{
+        console.log(row)
+    })
 })
