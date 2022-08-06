49a0ddb8041062d7d7b88fe1c66a440da87c8fa5
Amit Assa <amitassa9@gmail.com>
2022-08-06 20:53:00 +0300

    create, drop, insert

---
+++ sqlite-play.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let sql;
+
+const db = new sqlite3.Database("./mock.db", sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message);
+    console.log('connection successful');
+});
+
+// create table
+
+// sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY, first_name, last_name, username, password)'
+// db.run(sql)
+
+// drop table
+// db.run("DROP TABLE users")
+// db.close();
+
+// insert data into table
+
+sql = 'INSERT INTO users (first_name, last_name, username, password) VALUES (?,?,?,?)'
+db.run(sql, ["amit", "assa", "kingston", "12345"], (err) => {
+    if (err) return console.error(err.message);
+})
